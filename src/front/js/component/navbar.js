acd9444630d600d74f6108dbbf65c99b09178de6
Beverly M <bevponi@gmail.com>
2023-06-10 18:38:11 +0000

    Login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Login from "./login.js"
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
+				<Login />
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
